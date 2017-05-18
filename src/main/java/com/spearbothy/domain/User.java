02813e59a543e4c774f2679e688e391565600a54
AlexMaHao <zziamahao@163.com>
2017-05-18 22:59:21 +0800

    添加lombok插件与依赖，减少实体类的编写

--- src/main/java/com/spearbothy/domain/User.java
+++ src/main/java/com/spearbothy/domain/User.java
@@ -1,35 +1,20 @@
 package com.spearbothy.domain;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.Accessors;
+
 /**
  * Created by Alex_MaHao on 2017/5/17.
  */
+@Accessors(chain = true)
+@NoArgsConstructor
+@Getter
+@Setter
 public class User {
 
     private String user_id;
     private String password;
     private String username;
-
-    public String getUser_id() {
-        return user_id;
-    }
-
-    public void setUser_id(String user_id) {
-        this.user_id = user_id;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
 }
